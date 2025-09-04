2d134e994e0870edb177dde03b87e3fbda345f99
Vivek <30vivekgoyal@gmail.com>
2025-09-04 10:24:57 +0530

    Add provider configuration for Azure in Dev environment

--- Environment/Dev/infra.provider.tf
+++ Environment/Dev/infra.provider.tf
@@ -14,4 +14,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
+  use_oidc        = true
+  subscription_id = "879e262b-becf-4bea-a163-835b023ba8e0"
+  tenant_id       = "749c1dd0-8da4-446d-987c-6deb1b6cbab9"
+  client_id       = "64060588-2a5a-4f40-a63b-c232de8f5cde"
 }
