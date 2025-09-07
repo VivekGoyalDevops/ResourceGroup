08038322b7303816fe16ce0a7f91706075e91ac5
Vivek <30vivekgoyal@gmail.com>
2025-09-07 22:30:43 +0530

    Fix incorrect property names in Azure backend configuration

--- Environment/Prod/infra.provider.tf
+++ Environment/Prod/infra.provider.tf
@@ -7,9 +7,9 @@ terraform {
   }
   backend "azurerm" {
     use_oidc = true
-    client-id = "c14045f6-2258-4728-bcee-9337b5e6da08"
-    tenant-id = "749c1dd0-8da4-446d-987c-6deb1b6cbab9"
-    subscription-id = "879e262b-becf-4bea-a163-835b023ba8e0"
+    client_id = "c14045f6-2258-4728-bcee-9337b5e6da08"
+    tenant_id = "749c1dd0-8da4-446d-987c-6deb1b6cbab9"
+    subscription_id = "879e262b-becf-4bea-a163-835b023ba8e0"
     resource_group_name   = "vivek-rg"
     storage_account_name  = "vivekstracc"
     container_name        = "vivekstracccontainer"
